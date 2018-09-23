571d918dd1b31c0cdbe2d8bfd5498f39b0341f7b
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 18:59:13 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan13/relational/PBO310117121Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan13.relational;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMK
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan proses relasional
+ */
+public class PBO310117121Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+
+    }
+    
+}
